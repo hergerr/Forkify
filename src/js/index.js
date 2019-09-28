c8fcdc9528b0411c519ced8fd83ed34a75203c72
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2019-09-28 18:42:45 +0200

    Added loader spinner

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,20 @@
 import Search from './models/Search'
-import { elements } from './views/base'
+import { elements, renderLoader, clearLoader } from './views/base'
 import * as searchView from './views/searchView'
 
 const state = {};
 
 const controlSearch = async() => {
     const query = searchView.getInput();
-    console.log(query);
 
     if(query){
         state.search = new Search(query);
-        await state.search.getResults();
         searchView.clearInput();
         searchView.clearResults();
+        renderLoader(elements.searchRes);
+        await state.search.getResults();
+
+        clearLoader();
         searchView.renderResults(state.search.recipes);
     }
 };
