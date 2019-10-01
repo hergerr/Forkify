5ed2e77f1d3191771b4f058ec45649f3cfc51bde
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2019-10-01 15:14:34 +0200

    Added recipe view

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ import Search from './models/Search'
 import Recipe from './models/Recipe'
 import {elements, renderLoader, clearLoader} from './views/base'
 import * as searchView from './views/searchView'
+import * as recipeView from './views/recipeView'
 
 const state = {};
 
@@ -45,8 +46,14 @@ elements.searchResPages.addEventListener('click', e => {
 const controlRecipe = async () => {
     const id = window.location.hash.replace('#', '');
 
-
     if (id) {
+        recipeView.clearRecipe();
+        renderLoader(elements.recipe);
+
+        if(state.search) {
+            searchView.highlightSelected(id);
+        }
+
         state.recipe = new Recipe(id);
 
         try{
@@ -56,8 +63,8 @@ const controlRecipe = async () => {
             state.recipe.calcTime();
             state.recipe.calcServings();
 
-            console.log(state.recipe);
-
+            clearLoader();
+            recipeView.renderRecipe(state.recipe);
         } catch (e) {
             alert('Error processing recipe');
         }
